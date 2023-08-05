0352b013d311983a7753a3650b9c410f50eb754e
LordRyanII <ryanoliveiram2020@outlook.com>
2023-08-05 16:18:33 -0300

    Criando estruturas do projeto

--- app.js
+++ app.js
@@ -1,3 +1,18 @@
-export default () => {
-  console.log('Hello Word!')
+import { Express } from "express";
+class App {
+  constructor(){
+    this.app = Express();
+    this.middlewares();
+    this.routes();
+
+  }
+  middlewares () {
+    this.app.use(Express.urlencoded({extends : true}));
+    this.app.use(Express.json());
+  };
+  routes () {
+
+  };
 }
+
+export default new App().app;
